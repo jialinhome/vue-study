22178a529a8dc57e1a01ddd509f5dbfce7604068
jjl <jjlin2015@163.com>
2017-02-07 18:19:34 +0800

    第一次修改

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div">
-    <v-header></v-header>
-    <div class="tab">
+  <div>
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
       <div class="tab-item">
         <a v-link="{path:'/goods'}">商品</a>
       </div>
@@ -23,6 +23,20 @@
   import header from './components/header/header';
 
   export default {
+    data () {
+      return {
+        seller: {},
+      };
+    },
+    created () {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body;
+        if (response.errno === 0) {
+          this.seller = response.data;
+          console.log(this.seller);
+        }
+      });
+    },
     components: {
       'v-header': header,
     },
@@ -31,13 +45,21 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "./commen/stylus/mixin.styl"
+
   .tab
     display: flex
     width: 100%
     height: 40px
     line-height: 40px
+    border-1px(rgba(7, 17, 27, 0.1))
     .tab-item
       flex: 1
       text-align: center
-
+      & > a // .tab-item下的直接a子元素
+        display: block /*目的是点击a标签边上的空白地方也可以实现跳转，a元素会自动撑满父元素*/
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.active // a元素中有active类的元素
+          color: rgb(240, 20, 20)
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App';
 import goods from './components/goods/goods';
+import ratings from './components/ratings/ratings';
+import seller from './components/seller/seller';
+
+import './commen/stylus/base.styl'; // 引入全局stylus文件
 
 Vue.use(VueRouter);
+Vue.use(VueResource); // 全局注册VueResource
 
 // /* eslint-disable no-new */
 // new Vue({
@@ -14,12 +20,22 @@ Vue.use(VueRouter);
 // });
 const app = Vue.extend(App);
 
-const router = new VueRouter();
+const router = new VueRouter({
+  linkActiveClass: 'active', // 给被选中的item配置类名称
+});
 
 router.map({
   '/goods': {
     component: goods,
   },
+  '/ratings': {
+    component: ratings,
+  },
+  '/seller': {
+    component: seller,
+  },
 });
 
 router.start(app, '#app');
+
+router.go('/goods');
